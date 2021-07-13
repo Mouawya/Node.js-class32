48c5a78de3647be0e7a3b43c87d128f7755a626a
Mouawya <mouawya88@gmail.com>
2021-07-13 10:54:50 +0200

    deleted padLeft.js from ex2 and formatted the scripts

--- week1/homework/HackYourTemperature/server.js
+++ week1/homework/HackYourTemperature/server.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello from backend to frontend!');
+app.get("/", (req, res) => {
+  res.send("Hello from backend to frontend!");
 });
 
 app.listen(3000);
--- week1/homework/exercises/3-web-server/server.js
+++ week1/homework/exercises/3-web-server/server.js
@@ -2,37 +2,35 @@
  * Exercise 3: Create an HTTP web server
  */
 
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 
 //create a server
 let server = http.createServer(function (req, res) {
   // YOUR CODE GOES IN HERE
-	if(req.url === '/'){
-		res.writeHead(200, {'Content-Type': 'text/html'});
-	fs.readFile('index.html', (err, data) => {
-		if(err){
-			res.writeHead(404)
-		}else {
-			res.write(data)
-		}
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    fs.readFile("index.html", (err, data) => {
+      if (err) {
+        res.writeHead(404);
+      } else {
+        res.write(data);
+      }
 
-		res.end()
-	});
-	}
-	if(req.url === '/index.js'){
-		res.writeHead(200, {'Content-Type': 'text/javascript'})
-		fs.readFile('index.js', (err, data) => {
-			if(err){
-				res.writeHead(404)
-			}else{
-				res.write(data)
-			}
-			res.end()
-		})
-	}
-	
-
+      res.end();
+    });
+  }
+  if (req.url === "/index.js") {
+    res.writeHead(200, { "Content-Type": "text/javascript" });
+    fs.readFile("index.js", (err, data) => {
+      if (err) {
+        res.writeHead(404);
+      } else {
+        res.write(data);
+      }
+      res.end();
+    });
+  }
 });
 
 server.listen(3000); // The server starts to listen on port 3000
